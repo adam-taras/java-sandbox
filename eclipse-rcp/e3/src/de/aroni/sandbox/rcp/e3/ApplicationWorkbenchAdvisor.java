08784e330b19fd572f508842c56fb483351c569e
Adam Taras <dev@adam-taras.de>
2016-01-18 10:48:11 +0100

    Created a new plugin project based on Eclipse RCP 3.x using the wizard

---
+++ eclipse-rcp/e3/src/de/aroni/sandbox/rcp/e3/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package de.aroni.sandbox.rcp.e3;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "de.aroni.sandbox.rcp.e3.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
